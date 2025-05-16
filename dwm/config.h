ff362a10445830a71606a1ef8135072edd1db5cc
Srabon <totalblank@yahoo.com>
2025-05-16 23:57:38 +0600

    Added bookmarking Mod4 + BackSpace to show all the bookmarks and Mod4 + Shift + BackSpace to add the selected link to bookmarks

--- dwm/config.h
+++ dwm/config.h
@@ -60,41 +60,47 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+/* custom commands variable list */
+static const char bookmarks[] = "xdotool type $(grep -v '^#' ~/dotfiles/bookmarks | dmenu -fn JetBrainMonoNerdFontMono:size=14 -b -i -l 50 | cut -d' ' -f1)";
+static const char add_bookmark[] = "$HOME/dotfiles/bash_scripts/bookmarkthis";
+
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* modifier          key        	function        argument */
+  { MODKEY,            XK_r,      	spawn,          {.v = dmenucmd } },
+  { MODKEY|ShiftMask,  XK_Return, 	spawn,          {.v = termcmd } },
+  { MODKEY,            XK_BackSpace, 	spawn,       SHCMD(bookmarks) },
+  { MODKEY|ShiftMask,  XK_BackSpace, 	spawn,       SHCMD(add_bookmark) },
+  { MODKEY,            XK_b,      	togglebar,      {0} },
+  { MODKEY,            XK_j,      	focusstack,     {.i = +1 } },
+  { MODKEY,            XK_k,      	focusstack,     {.i = -1 } },
+  { MODKEY,            XK_i,      	incnmaster,     {.i = +1 } },
+  { MODKEY,            XK_d,      	incnmaster,     {.i = -1 } },
+  { MODKEY,            XK_h,      	setmfact,       {.f = -0.05} },
+  { MODKEY,            XK_l,      	setmfact,       {.f = +0.05} },
+  { MODKEY,            XK_Return, 	zoom,           {0} },
+  { MODKEY,            XK_Tab,    	view,           {0} },
+  { MODKEY|ShiftMask,  XK_c,      	killclient,     {0} },
+  { MODKEY,            XK_t,      	setlayout,      {.v = &layouts[0]} },
+  { MODKEY,            XK_f,      	setlayout,      {.v = &layouts[1]} },
+  { MODKEY,            XK_m,      	setlayout,      {.v = &layouts[2]} },
+  { MODKEY,            XK_space,  	setlayout,      {0} },
+  { MODKEY|ShiftMask,  XK_space,  	togglefloating, {0} },
+  { MODKEY,            XK_0,      	view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,  XK_0,      	tag,            {.ui = ~0 } },
+  { MODKEY,            XK_comma,  	focusmon,       {.i = -1 } },
+  { MODKEY,            XK_period, 	focusmon,       {.i = +1 } },
+  { MODKEY|ShiftMask,  XK_comma,  	tagmon,         {.i = -1 } },
+  { MODKEY|ShiftMask,  XK_period, 	tagmon,         {.i = +1 } },
+  TAGKEYS(             XK_1,      	                0)
+  TAGKEYS(             XK_2,      	                1)
+  TAGKEYS(             XK_3,      	                2)
+  TAGKEYS(             XK_4,      	                3)
+  TAGKEYS(             XK_5,      	                4)
+  TAGKEYS(             XK_6,      	                5)
+  TAGKEYS(             XK_7,      	                6)
+  TAGKEYS(             XK_8,      	                7)
+  TAGKEYS(             XK_9,      	                8)
+  { MODKEY|ShiftMask,  XK_q,      	quit,           {0} },
 };
 
 /* button definitions */
