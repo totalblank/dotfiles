c442b43cf0ca61af34ec252d6dfc9ba58bb88554
Srabon <totalblank@yahoo.com>
2025-07-03 17:02:46 +0600

    Added slstatus config

--- nvim/lua/plugins/r.lua
+++ nvim/lua/plugins/r.lua
@@ -53,4 +53,40 @@ return {
       vim.keymap.set("n", "gzc", "<Plug>SlimeConfig", { remap = true, silent = false })
     end,
   },
+  { -- preview equations
+    "jbyuki/nabla.nvim",
+    keys = {
+      { "<leader>qm", ':lua require"nabla".toggle_virt()<cr>', desc = "toggle [m]ath equations" },
+    },
+  },
+  { -- paste an image from the clipboard or drag-and-drop
+    "HakonHarnes/img-clip.nvim",
+    event = "BufEnter",
+    ft = { "markdown", "quarto", "latex" },
+    opts = {
+      default = {
+        dir_path = "img",
+      },
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+        quarto = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("img-clip").setup(opts)
+      vim.keymap.set("n", "<leader>ii", ":PasteImage<cr>", { desc = "insert [i]mage from clipboard" })
+    end,
+  },
 }
