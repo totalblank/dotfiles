c0b54622719b1f1ffc2c32609f6ff38f51cebf11
srabon <totalblank@yahoo.com>
2026-01-20 12:56:55 +0600

    added fuzzy things

--- nvim/lua/fzf_config.lua
+++ nvim/lua/fzf_config.lua
@@ -1,4 +1,17 @@
-require('fzf-lua').setup({
+local fzf_lua = require('fzf-lua')
+
+fzf_lua.setup({
   "telescope",                  -- makes it look and behave like Telescope
   winopts = { preview = { layout = "vertical" } },
+
+  fzf_opts = {
+    ["--preview-window"] = "right:60%:hidden", -- Customize preview window
+  }
 })
+
+-- Define key mappings
+vim.api.nvim_set_keymap('n', '<Leader>f', ':FzfLua files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>b', ':FzfLua buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>o', ':FzfLua oldfiles<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>g', ':FzfLua live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>p', ':FzfLua find_files<CR>', { noremap = true, silent = true })
